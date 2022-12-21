593b0baa57d479bb54f460d9d0131b29d985bb37
Olusolaayomi <sshollybaby1@gmail.com>
2022-12-21 04:34:52 -0800

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 ''' A Module that creates a Square object '''
 
+
 class Square:
     ''' Creating an Object template '''
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         '''
             The init method initializes the class instance
 
